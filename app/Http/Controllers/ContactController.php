f5c1660dc8c66922d717f17cae90930fda6e2220
masato hara <hr.masato@gmail.com>
2026-09-21 17:00:12 +0900

    Initial commit

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        $tags = Tag::all();
+
+        return view('contact.index', compact('categories', 'tags'));
+    }
+}
